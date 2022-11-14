005d1049d2f8f16806a8c1781ab32ec388ee21dc
Valdor <example@gmai.com>
2022-11-14 13:39:33 +0300

    Add modul strings and change nums

--- first/nums.py
+++ first/nums.py
@@ -2,7 +2,8 @@ def plus(a, b):
     return a + b
 
 
-text = "new text"
+def mul(a, b):
+    return a * b
 
 
 def minus(a, b):
@@ -12,3 +13,4 @@ def minus(a, b):
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(6, 5))
+    print(mul(3, 3))

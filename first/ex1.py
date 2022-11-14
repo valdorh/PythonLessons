e6623fb315df5a679fc0415a9307a1777996af92
valdorh <doroninva@yandex.ru>
2022-11-15 00:34:50 +0300

    Add modul ex1 and dictan

---
+++ first/ex1.py
@@ -0,0 +1,6 @@
+d = {"house": "дом", "car": "иашина",
+     "tree": "дерево", "road": "дорога",
+     "river": "река"}
+print(d)
+dc = dict(one = 1, two = 2, three=3, four=4)
+print(dc)
--- first/nums.py
+++ first/nums.py
@@ -9,8 +9,3 @@ def mul(a, b):
 def minus(a, b):
     return a - b
 
-
-if __name__ == '__main__':
-    print(plus(2, 2))
-    print(minus(6, 5))
-    print(mul(3, 3))

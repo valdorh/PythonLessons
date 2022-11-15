adc80a3151e2884e339cd2d08e268d6e4c730cc6
valdorh <doroninva@yandex.ru>
2022-11-15 12:05:33 +0300

    Lesson 1 Dict

--- first/ex1.py
+++ first/ex1.py
@@ -1,7 +1,2 @@
-inp = "one=1 two=2 three=3"
-lst = list(inp.split())
-tmp_lst = [y.split("=") for y in lst]
-for i in range(len(tmp_lst)):
-     tmp_lst[i][1] = int(tmp_lst[i][1])
-d = dict(tmp_lst)
-print(*sorted(d.items()))
+from lib_lessons import *
+learn_dict_29()
---
+++ first/lib_lessons.py
@@ -0,0 +1,8 @@
+def learn_dict_29():
+    inp = "one=1 two=2 three=3"
+    lst = list(inp.split())
+    tmp_lst = [y.split("=") for y in lst]
+    for i in range(len(tmp_lst)):
+        tmp_lst[i][1] = int(tmp_lst[i][1])
+    d = dict(tmp_lst)
+    print(*sorted(d.items()))

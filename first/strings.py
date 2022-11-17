4f5a584f53ce08e6a927f9a74889dbe20e04b8f0
valdorh <doroninva@yandex.ru>
2022-11-17 23:40:56 +0300

    Change strings.py

--- first/strings.py
+++ first/strings.py
@@ -1,13 +1,16 @@
 import datetime
+import typing
 
 
-def up(text):
+def up(text: str) -> typing.Any:
     return text.upper()
 
 
 if __name__ == '__main__':
     result = 121583568563862032089
     pi = 3.141592
+    s = up("pi")
+    print(s)
     s = f"{result:,.2f}"
     today = datetime.datetime.now()
     s1 = f"Today: {today:%d-%m-%Y  %H:%M}"

87f2d448fbfed2fcb8005a418e576a9a64e90ef9
valdorh <doroninva@yandex.ru>
2022-11-17 23:30:40 +0300

    Delete main.py

--- first/main.py
+++
@@ -1,4 +0,0 @@
-from first.nums import plus
-
-if __name__ == "__main__":
-    print(plus(2, 4))
--- first/strings.py
+++ first/strings.py
@@ -1,2 +1,15 @@
+import datetime
+
+
 def up(text):
     return text.upper()
+
+
+if __name__ == '__main__':
+    result = 121583568563862032089
+    pi = 3.141592
+    s = f"{result:,.2f}"
+    today = datetime.datetime.now()
+    s1 = f"Today: {today:%d-%m-%Y  %H:%M}"
+    print(s)
+    print(s1)

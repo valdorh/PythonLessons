14e8671d7e1b4bbeb7b9697e684da8639e011cdb
valdorh <doroninva@yandex.ru>
2022-11-18 09:08:10 +0300

    Begin mistakes Lesson

--- first/bookeans.py
+++ first/bookeans.py
@@ -11,5 +11,5 @@ falsy = [None, False, 0, 0.0, [], {}, set(), tuple(), range(0), '']
 if check(1) and check(2) and check(-3) and check(6):
     print("YES")
 print('='*10)
-if check(3) or check(0) or check(-3) or check(6):
+if check(-3) or check(0) or check(-3) or check(6):
     print("YES")
